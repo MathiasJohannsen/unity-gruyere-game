94d2b5063a42a3c2ecfd15c7694e05c3b4d14f6e
Mathias Johannsen <m.johannsen.privat@gmail.com>
2022-06-06 02:52:56 +0200

    added handleSpeed variable

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -6,27 +6,32 @@ public class MovementScript : MonoBehaviour
 {
     public Transform leftHandle;
     public Transform rightHandle;
-    
+    public float handleSpeed = 0.8f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.W)) {
-            leftHandle.Translate(Vector3.up * Time.deltaTime);
+        if (Input.GetKey(KeyCode.W))
+        {
+            leftHandle.Translate(Vector3.up * Time.deltaTime * handleSpeed);
         }
-        if(Input.GetKey(KeyCode.S)) {
-            leftHandle.Translate(Vector3.down * Time.deltaTime);
+        if (Input.GetKey(KeyCode.S))
+        {
+            leftHandle.Translate(Vector3.down * Time.deltaTime * handleSpeed);
         }
-        if(Input.GetKey(KeyCode.UpArrow)) {
-            rightHandle.Translate(Vector3.up * Time.deltaTime);
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rightHandle.Translate(Vector3.up * Time.deltaTime * handleSpeed);
         }
-        if(Input.GetKey(KeyCode.DownArrow)) {
-            rightHandle.Translate(Vector3.down * Time.deltaTime);
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rightHandle.Translate(Vector3.down * Time.deltaTime * handleSpeed);
         }
     }
 }
